35368be4e64c6a42a8f603c340d70b5d8676d6d0
akhil5.gupta@ril.com <akhil5.gupta@ril.com>
2024-03-12 17:24:09 +0530

    first code version.

---
+++ app.py
@@ -0,0 +1,86 @@
+import os
+from flask import Flask,request, jsonify
+from werkzeug.utils import secure_filename
+from utils import f_resize_raw_image, f_predict, f_clear_folder
+from flask_cors import CORS
+#import jsonify
+from tensorflow.keras.preprocessing.image import ImageDataGenerator 
+import scipy
+
+app=Flask(__name__)
+CORS(app)
+cors = CORS(app, resource={r"/*": {"origins": "*"}})
+
+
+UPLOAD_FOLDER = "uploads"
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+
+TMP_FOLDER = "tmp"
+if not os.path.exists(TMP_FOLDER):
+    os.makedirs(TMP_FOLDER)
+
+TMP_FOLDER0 = os.path.join(TMP_FOLDER, '0')
+if not os.path.exists(TMP_FOLDER0):
+    os.makedirs(TMP_FOLDER0)
+
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+app.config["TMP_FOLDER"] = TMP_FOLDER
+
+BASE_URL = os.environ.get("BASE_URL", "http://localhost:5003")
+PORT = os.environ.get("PORT", 5003)
+
+target_size = (224,224)
+
+@app.route("/obstruction_detection/process-image", methods = ['POST'])
+def upload_file():
+    if "file" not in request.files:
+        return "No file part"
+    
+    image_file = request.files["file"]
+    if image_file.filename == "":
+        return "No selected image file"
+
+    filename = secure_filename(image_file.filename)
+    if image_file:
+        image_file_path = os.path.join(UPLOAD_FOLDER, filename)
+        image_file.save(image_file_path)
+        
+    
+    if not (image_file_path.endswith(".jpg") or image_file_path.endswith(".jpeg") or image_file_path.endswith(".png")):
+        return (
+            jsonify(
+                {
+                    "message": "Incorrect file format of image file. Allowed only (jpg and png)",
+                    "success": False,
+                }
+            ),
+            200,
+        )
+    
+    # Picking the image from ./uploads directory, resizing the image to target_size and pushing the resized images to tmp directory.
+    
+    f_resize_raw_image(UPLOAD_FOLDER, TMP_FOLDER0, target_size)
+
+    # Creating data generator
+    image_datagen = ImageDataGenerator( rescale = 1.0/255. )
+    image_generator = image_datagen.flow_from_directory(TMP_FOLDER, batch_size=10, class_mode='binary', target_size=target_size)
+    prediction  = f_predict(image_generator)
+    f_clear_folder(TMP_FOLDER0)
+    f_clear_folder(UPLOAD_FOLDER)
+
+    return (
+            jsonify(
+                {
+                    "message": f"Image processed sucessfully",
+                    "success": True,
+                    "Obstruction_dectected": prediction,
+                }
+            ),
+            200,
+        )
+
+
+if __name__ =="__main__":
+    app.run(debug=True, port = PORT, host="0.0.0.0")
---
+++ utils.py
@@ -0,0 +1,45 @@
+import os
+from PIL import Image
+import pickle
+import tensorflow as tf
+
+model = pickle.load(open("model/model_efficientnet_v1.pkl",'rb'))
+
+
+def f_resize_raw_image(src_dir, tgt_dir, target_size):
+    
+    ls_src_dir_files = [ os.path.join(src_dir, i) for i in os.listdir(src_dir)]
+    try:
+        ls_src_dir_files.remove(src_dir+'/.DS_Store')
+    except:
+        print('Not removed')
+
+    for i, img_path in enumerate(ls_src_dir_files):
+        print(img_path)
+        print(f'[+] Processing file {img_path}...')
+        image = Image.open(img_path)
+        image = image.convert('RGB') #convert png to jpg
+        a = image.resize(target_size)
+        a.save(tgt_dir+'/'+str(i)+'.jpg')
+
+
+def f_predict(image_generator):
+    pred_hist = model.predict(image_generator)
+    print(pred_hist)
+    if pred_hist[0][0]<0.5:
+        return False
+    else:
+        return True
+
+
+def f_clear_folder(folder_path):
+    # List all files in the folder
+    files = os.listdir(folder_path)
+    
+    # Iterate over each file and delete it
+    for file_name in files:
+        file_path = os.path.join(folder_path, file_name)
+        # Check if it's a file (not a directory) before attempting to delete
+        if os.path.isfile(file_path):
+            os.remove(file_path)
+            #print(f"Deleted: {file_path}")
